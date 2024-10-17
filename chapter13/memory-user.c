513ab4cfd63faae632f6c071c1304b54d058af8f
philippeZim <philippe.zim22@gmail.com>
2024-10-17 18:46:37 +0200

    chapter 13 nr. 1

--- chapter13/memory-user.c
+++ chapter13/memory-user.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 
 char *reserve_memory(int megabytes) {
     char *res = malloc(megabytes * sizeof(char) * 1000000);
@@ -9,23 +13,56 @@ char *reserve_memory(int megabytes) {
     }
 }
 
+
 int main(int argc, char **argv) {
-    if (argc < 2 || argc > 3) {
+    if (argc != 2 && argc != 3 && argc != 5) {
         printf("Wrong number of parameters use -h for help.");
         return 1;
     }
 
-    if (strcmp(argv[1], "-h") == 0) {
-        char *help_str = "Usage:\n-h -> help\n-n [number of megabytes]\n-t [runtime]\n";
-        printf("%s", help_str);
+    clock_t start_time = clock();
+    int megabytes;
+    int time_in_ms = -1;
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            char *help_str = "Usage:\n-h -> help\n-n [number of megabytes]\n-t [runtime in ms]\n";
+            printf("%s", help_str);
+            return 1;
+        } else if (strcmp(argv[i], "-n") == 0) {
+            megabytes = atoi(argv[i+1]);
+            i++;
+        } else if (strcmp(argv[i], "-t") == 0) {
+            time_in_ms = atoi(argv[i+1]);
+            i++;
+        } else {
+            printf("Error parsing parameters, use -h for help");
+            return 1;
+        }
     }
-    
-    int megabytes = 0;
 
     char *mem = reserve_memory(megabytes);
     if (mem == NULL) {
         return 1;
     }
+    if (time_in_ms == -1) {
+        while(1) {
+            for (int i = 0; i < megabytes * 1000000; i++) {
+                printf("%c", mem[i]);
+            }
+        }
+    } else {
+        while(1) {
+            for (int i = 0; i < megabytes * 1000000; i++) {
+                printf("%c", mem[i]);
+
+                if ((((double) (clock() - start_time)) / CLOCKS_PER_SEC) * 1000 >= time_in_ms) {
+                    return 0;
+                } 
+            }
+        }
+    }
+    
 
     return 0;
 }
